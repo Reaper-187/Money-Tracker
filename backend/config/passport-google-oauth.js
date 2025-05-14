a9125846c6c4cba8ff8a2eb1eae25bacd5f48a2d
Reaper-187 <Abdulkader_cheik@outlook.de>
2025-05-14 23:12:58 +0200

    remove wrong credentials

--- backend/config/passport-google-oauth.js
+++ backend/config/passport-google-oauth.js
@@ -1,6 +1,5 @@
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const User = require("../model/userSchema/userModel");
-axios.defaults.withCredentials = true;
 
 const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
